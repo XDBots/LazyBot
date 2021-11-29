fd8fc70832e92b9157cd5e0bd9e6aa19cb488218
ArnabXD <arnabxd@pm.me>
2021-11-29 19:37:09 +0530

    File Helper

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import { client, startUserbot, plugins } from './userbot';
+import { LazyFileHelper } from './utils';
 
 (async () => {
+  await LazyFileHelper.init();
   await startUserbot();
   await plugins.load(client);
 })();
---
+++ src/utils/fileHelper.ts
@@ -0,0 +1,37 @@
+import fs from 'fs';
+import path from 'path';
+
+class FileHelper {
+  constructor() {}
+
+  async init() {
+    fs.mkdir(
+      path.join(process.cwd(), 'downloads'),
+      { recursive: true },
+      (error) => {
+        error &&
+          console.error('[LazyGram]' + `[${error.name}] => ` + error.message);
+      }
+    );
+  }
+
+  saveFileFromBuffer(data: Buffer, filename: string) {
+    fs.writeFile(
+      path.join(process.cwd(), 'downloads', filename),
+      data,
+      (error) => {
+        error &&
+          console.error('[LazyGram]' + `[${error.name}] => ` + error.message);
+      }
+    );
+  }
+
+  deleteDownloaded(filename: string) {
+    fs.rm(path.join(process.cwd(), 'downloads', filename), (error) => {
+      error &&
+        console.error('[LazyGram]' + `[${error.name}] => ` + error.message);
+    });
+  }
+}
+
+export const LazyFileHelper = new FileHelper();
