80525652621f3b9017c72bda870a98e1354dc375
ArnabXD <arnabxd@pm.me>
2021-11-30 05:23:08 +0530

    db uri var

--- src/env.ts
+++ src/env.ts
@@ -3,32 +3,30 @@ import { cleanEnv, str, num } from 'envalid';
 dotenv.config({});
 
 export default cleanEnv(process.env, {
-  // Telegram APP API ID
-  API_ID: num(),
-
-  // Telegram APP API Hash
-  API_HASH: str(),
-
-  // GramJS Session ; https://sg.rojser.best
-  STRING_SESSION: str(),
-
-  // GramJS Maximum Connection Retries Limit
-  CONNECTION_RETRIES: num({ default: 5 }),
-
-  // GramJS Log Level
-  LOG_LEVEL: str({ choices: ['none', 'debug', 'error'], default: 'none' }),
-
-  // Userbot Command Prefix
-  CMD_PREFIX: str({ default: '.' }),
-
-  // Postgres DB Connectrion URI
-  POSTGRES_URI: str(),
+  API_ID: num({
+    desc: "Your telegram app's API_ID. Get it from https://my.telegram.org/apps"
+  }),
+  API_HASH: str({
+    desc: "Your telegram app's API_ID. Get it from https://my.telegram.org/apps"
+  }),
+  STRING_SESSION: str({
+    desc: 'GramJS/Telethon session string of the VC User.'
+  }),
+  CONNECTION_RETRIES: num({
+    default: 5,
+    desc: 'GramJS maximum connection retries limit'
+  }),
+  LOG_LEVEL: str({
+    choices: ['none', 'debug', 'error'],
+    default: 'none',
+    desc: 'GramJS Log Level'
+  }),
+  CMD_PREFIX: str({ default: '.', desc: 'Userbot Command Prefix' }),
+  DATABASE_URL: str({ desc: 'Postgres database connection string' }),
 
   PM_PERMIT_MODE: str({ choices: ['text', 'media'], default: 'text' }),
-  // Media to Use in PM Permit
   PM_PERMIT_MEDIA: str({
     default: 'https://telegra.ph/file/830d75f72e02ca97f7242.jpg'
   }),
-
   PM_PERMIT_MAX_WARNS: num({ default: 3 })
 });
