70bdaa22be25f2d51b91c111549f03ce11ea678c
ArnabXD <arnabxd@pm.me>
2021-11-29 19:37:38 +0530

    GetFullUser

--- src/utils/gramHelpers.ts
+++ src/utils/gramHelpers.ts
@@ -1,7 +1,14 @@
-import { Api } from 'telegram';
+import { Api, TelegramClient } from 'telegram';
 
 export const BlockUser = (id: number) => new Api.contacts.Block({ id });
-export const GetFullUser = (id: number) =>
-  new Api.users.GetFullUser({
-    id
-  });
+export const GetFullUser = async (
+  id: number | string,
+  client: TelegramClient
+) => {
+  if (typeof id === 'string' && /^\d+$/.test(id)) {
+    id = parseInt(id);
+  }
+
+  const entity = await client.getEntity(id);
+  return await client.invoke(new Api.users.GetFullUser({ id: entity }));
+};
