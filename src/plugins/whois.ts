3c73803c357c9dd3522f9d54fd3fd701d98e715b
ArnabXD <arnabxd@pm.me>
2021-11-30 06:22:10 +0530

    Better whois + replied msg support

--- src/plugins/whois.ts
+++ src/plugins/whois.ts
@@ -2,15 +2,25 @@ import { Api } from 'telegram';
 import { extract, GetFullUser, LazyFileHelper } from '../utils';
 import { escape } from 'html-escaper';
 
-const getUserInfoText = (user: Api.User) => {
-  user.firstName ? user.firstName.replace('\u2060', '') : (user.firstName = '');
-  user.lastName ? user.lastName.replace('\u2060', '') : (user.lastName = '');
+const getUserInfoText = (fulluser: Api.UserFull) => {
+  const user = fulluser.user as Api.User;
+  const fName = user.firstName ? user.firstName.replace('\u2060', '') : '';
+  const lName = user.lastName ? user.lastName.replace('\u2060', '') : '';
+  const uName = user.username ? '@' + user.username : '<code>None</code>';
+  const dcId =
+    user.photo && 'dcId' in user.photo ? user.photo.dcId : "Can't check";
 
   return (
-    `<b>First Name &#10143; </b><code>${escape(user.firstName)}</code>\n` +
-    `<b>Last Name &#10143; </b><code>${escape(user.lastName)}</code>\n` +
-    `<b>Username &#10143; </b>${user.username ? '@' + user.username : ''}\n` +
-    `<b>User ID &#10143; </b><code>${user.id}</code>`
+    `<b>First Name &#10143; </b><code>${escape(fName)}</code>\n` +
+    `<b>Last Name &#10143; </b><code>${escape(lName)}</code>\n` +
+    `<b>Username &#10143; </b>${uName}\n` +
+    `<b>User ID &#10143; </b><code>${user.id}</code>\n` +
+    `<b>DC ID &#10143; </b><code>${dcId}</code>\n` +
+    `<b>Bio &#10143; </b><code>${fulluser.about}</code>\n` +
+    `<b>Restricted &#10143; </b><code>${user.restricted}</code>\n` +
+    `<b>Verified &#10143; </b><code>${user.verified}</code>\n` +
+    `<b>Scammer &#10143; </b><code>${user.verified}</code>\n` +
+    `<b>Bot &#10143; </b><code>${user.bot}</code>\n`
   );
 };
 
@@ -19,20 +29,30 @@ const WHOIS: LGPlugin = {
     const { args } = extract(event.message.message);
 
     let userid: number | string = 'me';
+
     if (event.isPrivate) {
       userid = event.chatId!;
     }
+
     if (args) {
       userid = args;
     }
 
+    if (event.message.replyToMsgId) {
+      const repliedToMessage = await event.message.getReplyMessage();
+      if (repliedToMessage) {
+        // @ts-ignore
+        userid = repliedToMessage.fromId.userId;
+      }
+    }
+
     const fulluser = await GetFullUser(userid, client);
-    const user = fulluser.user as Api.User;
-    const image = await client.downloadProfilePhoto(user, {
+
+    const image = await client.downloadProfilePhoto(fulluser.user, {
       isBig: true
     });
 
-    const filename = 'pfp' + user.id.toString() + '.jpg';
+    const filename = 'pfp' + fulluser.user.id.toString() + '.jpg';
     LazyFileHelper.saveFileFromBuffer(image, filename);
     await event.message.delete({ revoke: false });
 
@@ -41,13 +61,13 @@ const WHOIS: LGPlugin = {
       await client.sendFile(event.chatId!, {
         file: `./downloads/${filename}`,
         forceDocument: false,
-        caption: getUserInfoText(user)
+        caption: getUserInfoText(fulluser)
       });
       LazyFileHelper.deleteDownloaded(filename);
     } else {
       // User doesn't have a profile pic
       await client.sendMessage(event.chatId!, {
-        message: getUserInfoText(user)
+        message: getUserInfoText(fulluser)
       });
     }
   },
@@ -57,7 +77,7 @@ const WHOIS: LGPlugin = {
 
 export default [WHOIS];
 export const help: LGHelp = (prefix) =>
-  `<b>${prefix}whois :</b> <code>Shows User Details</code>\n` +
-  `e.g:\n` +
+  `<i>Examples :</i>\n` +
   `<code>${prefix}whois</code>\n` +
-  `<code>${prefix}whois 777000</code>`;
+  `<code>${prefix}whois 777000</code>\n` +
+  `or reply <code>${prefix}whois</code> to someone's messages`;
