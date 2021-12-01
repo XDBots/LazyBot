db098af529cb5c4741506e7309e695f41484e704
ArnabXD <arnabxd@pm.me>
2021-12-01 14:31:03 +0530

    rearrange

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,8 +1,9 @@
+export * from './afk';
 export * from './files';
 export * from './gram';
-export * from './info';
-export * from './afk';
-export * from './regex';
 export * from './help';
+export * from './info';
 export * from './logger';
+export * from './pm2';
+export * from './regex';
 export { sleep } from 'telegram/Helpers';
