155395575928804c4783b451d5b56a20d1321757
ArnabXD <arnabxd@pm.me>
2021-12-04 12:40:33 +0530

    remove desc

--- src/env.ts
+++ src/env.ts
@@ -3,28 +3,19 @@ import { cleanEnv, str, num } from 'envalid';
 dotenv.config({});
 
 export default cleanEnv(process.env, {
-  API_ID: num({
-    desc: "Your telegram app's API_ID. Get it from https://my.telegram.org/apps"
-  }),
-  API_HASH: str({
-    desc: "Your telegram app's API_ID. Get it from https://my.telegram.org/apps"
-  }),
-  STRING_SESSION: str({
-    desc: 'GramJS/Telethon session string of the VC User.'
-  }),
+  API_ID: num(),
+  API_HASH: str(),
+  STRING_SESSION: str(),
   CONNECTION_RETRIES: num({
-    default: 5,
-    desc: 'GramJS maximum connection retries limit'
+    default: 5
   }),
   LOG_LEVEL: str({
     choices: ['none', 'debug', 'error'],
-    default: 'none',
-    desc: 'GramJS Log Level'
+    default: 'none'
   }),
-  CMD_PREFIX: str({ default: '.', desc: 'Userbot Command Prefix' }),
-  DATABASE_URL: str({ desc: 'Postgres database connection string' }),
+  CMD_PREFIX: str({ default: '.' }),
+  DATABASE_URL: str(),
   LOG_CHAT_ID: num(),
-
   PM_PERMIT_MODE: str({ choices: ['text', 'media'], default: 'text' }),
   PM_PERMIT_MEDIA: str({
     default: 'https://telegra.ph/file/830d75f72e02ca97f7242.jpg'
